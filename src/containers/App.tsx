6e35dfd40697502420b6befa69ad8b30752c030c
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-31 23:18:44 +0300

    use use-goals hook in App contaniner

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,16 +3,19 @@ import React, { FC, useEffect } from 'react';
 import '../styles/style.css';
 
 import useUser from '../hooks/use-user';
+import useGoals from '../hooks/use-goals';
 
 import { ConfigProvider, Root } from '@vkontakte/vkui';
 import Main from '../views/Main';
 
 const App: FC = () => {
     const { auth } = useUser();
+    const { fetchGoals } = useGoals();
 
     useEffect(() => {
         auth();
-    }, [auth]);
+        fetchGoals();
+    }, [auth, fetchGoals]);
 
     return (
         <ConfigProvider isWebView={true}>
