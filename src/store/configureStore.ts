47c74497cbcdc4ddeb261b6edb0eab1fb61415b7
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-31 20:57:31 +0300

    add configureStore

---
+++ src/store/configureStore.ts
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import api from '../middleware/api';
+import rootReducer, { AppState, userInitialState } from '../reducers';
+
+export const initialStore: AppState = {
+    user: userInitialState,
+};
+
+const composeEnhancers = composeWithDevTools({
+    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+});
+
+export default (preloadedState = initialStore) => createStore(
+    rootReducer,
+    preloadedState,
+    composeEnhancers(applyMiddleware(thunk, api))
+);
