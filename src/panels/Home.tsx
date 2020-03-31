7796fc95ef472c28c4332e0fb43b681329a5cb07
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-31 23:18:57 +0300

    change Home panel

--- src/panels/Home.tsx
+++ src/panels/Home.tsx
@@ -1,15 +1,77 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useCallback, useMemo } from 'react';
+import cn from 'classnames';
 
 import { PanelPrimary } from '../types/props';
 
-import { Panel, PanelHeaderSimple } from '@vkontakte/vkui';
+import useUser from '../hooks/use-user';
+import useGoals from '../hooks/use-goals';
+
+import { Panel, PanelHeaderSimple, Avatar, HorizontalScroll } from '@vkontakte/vkui';
+import Group from '../components/Group';
+import Button from '../components/Button';
 
 const Home: FC<PanelPrimary> = ({ id }: PanelPrimary) => {
+    const { pending, data } = useUser();
+    const { goalIds, goals } = useGoals();
+
+    const avatarView = useMemo(() => (!pending && data) &&
+        <Avatar
+            className="margin-purple--left"
+            size={32}
+            src={data.avatar200} />,
+        [pending, data]);
+
+    const [selectedGoalId, setGoalId] = useState<number | null>(null);
+
+    const setGoal = useCallback((e: any) => {
+        const catId = Number(e.target.dataset.catId);
+
+        if (isNaN(catId)) {
+            return;
+        }
+
+        setGoalId(catId);
+    }, []);
+
+    const resetGoal = useCallback(() => setGoalId(null), []);
+
+    const goalView = useCallback((id: number, index: number, array: any[]) =>
+        <div
+            key={id}
+            className={cn({
+                'padding-yellow--right': true,
+                'padding-blue--right': index === array.length - 1
+            })}>
+            <Button
+                priority="secondary"
+                size="small"
+                children={goals[id].title}
+                aria-pressed={selectedGoalId === id}
+                data-cat-id={id}
+                onClick={setGoal} />
+        </div>,
+        [goals, selectedGoalId, setGoal]);
+
+    const goalsView = useMemo(() => (Array.isArray(goalIds)) &&
+        <HorizontalScroll className="margin-pink--top margin-pink--bottom">
+            <Group start shrink>
+                <div className="padding-yellow--right padding-blue--left">
+                    <Button
+                        priority="secondary"
+                        size="small"
+                        children="Все люди"
+                        aria-pressed={selectedGoalId === null}
+                        onClick={resetGoal} />
+                </div>
+                {goalIds.map(goalView)}
+            </Group>
+        </HorizontalScroll>,
+        [goalIds, selectedGoalId, goalView, resetGoal]);
 
     return (
         <Panel id={id} separator={false}>
-            <PanelHeaderSimple children="Home" separator={false} />
-            Home
+            <PanelHeaderSimple left={avatarView} separator={false} />
+            {goalsView}
         </Panel>
     );
 };
