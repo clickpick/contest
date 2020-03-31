72cc1bb6e29bf2f6edb63a6cf8d09dc8edcfd5b9
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-31 21:43:18 +0300

    use use-user hook in App container

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,11 +1,19 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 
 import '../styles/style.css';
+
+import useUser from '../hooks/use-user';
 
 import { ConfigProvider, Root } from '@vkontakte/vkui';
 import Main from '../views/Main';
 
 const App: FC = () => {
+    const { auth } = useUser();
+
+    useEffect(() => {
+        auth();
+    }, [auth]);
+
     return (
         <ConfigProvider isWebView={true}>
             <Root activeView="main">
