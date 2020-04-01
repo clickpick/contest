504b060c4e239bdfae339b162c89f453dcc4b6cd
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-04-01 21:41:20 +0300

    change startedGoal schema

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,5 +3,16 @@ import { schema } from 'normalizr';
 export const goal = new schema.Entity('goals');
 export const arrayOfGoals = new schema.Array(goal);
 
-export const startedGoal = new schema.Entity('startedGoals');
+export const startedGoal = new schema.Entity(
+    'startedGoals',
+    { goalId: goal },
+    {
+        processStrategy: (entity) => {
+            const goalId = entity.goal;
+            delete entity.goal;
+
+            return { ...entity, goalId };
+        }
+    }
+);
 export const arrayOfStartedGoals = new schema.Array(startedGoal);
