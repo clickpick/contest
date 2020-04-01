88b029ffbf77f40c646fb8c23658908f5f9ed73f
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-04-01 23:30:58 +0300

    add Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,20 @@
+import React, { FC, HTMLAttributes, useMemo, memo } from 'react';
+import cn from 'classnames';
+
+import { HasChildren } from '../../types/props';
+
+export interface CardProps extends HTMLAttributes<HTMLDivElement>, HasChildren {
+    priority?: 'primary' | 'secondary'
+}
+
+const Card: FC<CardProps> = ({ className, priority, ...restProps }: CardProps) => {
+    const classNames = useMemo(() => cn(className, 'Card', `Card--${priority}`, 'Bs(bb)'), [className, priority]);
+
+    return <div className={classNames} {...restProps} />;
+};
+
+Card.defaultProps = {
+    priority: 'primary'
+};
+
+export default memo(Card);
